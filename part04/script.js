b9b9a73627d6c66fb110c297ec2b8de768401fe6
chittibasamsetti <chittibasamsetti@gmail.com>
2024-05-28 22:45:46 +0530

    add js assignments

---
+++ part04/script.js
@@ -0,0 +1,24 @@
+const create = document.getElementById("create");
+const backbutton = document.getElementById("back");
+const content = document.getElementById("content");
+const signup = document.getElementById("signup");
+
+create.addEventListener("click", function () {
+    content.classList.add("hidden");
+    signup.classList.remove("hidden");
+});
+
+backbutton.addEventListener("click", function () {
+    content.classList.remove("hidden");
+    signup.classList.add("hidden");
+});
+document.getElementById("submit").addEventListener("click",function(){
+    Email=document.getElementById("email").value;
+    correctMail="chittibasamsetti@gmail.com"
+      if(Email===correctMail){
+        alert("Welcome");
+      }
+      else{
+        alert("incorrect email or password");
+      }
+    })
